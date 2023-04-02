bee451c48170dc5bb827e3d56ee4130f0e8726df
Artyom Pionov <pionovartem11@gmail.com>
2023-04-02 22:37:59 +0100

    New Commit: Small change

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,10 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException { // <- IOException ignoring, no bueno. How to fix it will be learnt.
         System.out.println("Enter your message: ");
+        // OG User Input System
         BufferedReader inputReader = new BufferedReader(new InputStreamReader(System.in));
         String message = inputReader.readLine();
+        // Output
         System.out.println("Your message: " + message);
     }
 }
