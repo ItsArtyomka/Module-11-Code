9b9811356cde5a2ac602aa15a020ba596f1dc4f5
Artyom Pionov <pionovartem11@gmail.com>
2023-04-02 21:32:01 +0100

    New Commit:

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.IOException;
 
 // In this class, the OG way to get user's input is used and that's where I can learn about exceptions.
 public class Main {
     public static void main(String[] args) throws IOException { // <- IOException ignoring, no bueno.
         System.out.println("Enter your message: ");
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String message = reader.readLine();
+        BufferedReader inputReader = new BufferedReader(new InputStreamReader(System.in));
+        String message = inputReader.readLine();
         System.out.println("Your message: " + message);
     }
 }
